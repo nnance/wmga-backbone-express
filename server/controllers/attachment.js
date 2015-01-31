dd988614ebf57e794d5e0846f8043708ae2f32a9
Nick Nance <nance.nick@gmail.com>
2015-01-31 15:59:47 -0600

    express controllers

---
+++ server/controllers/attachment.js
@@ -0,0 +1,34 @@
+var fs 		= require('fs'),
+	AWS 	= require('aws-sdk'),
+	config 	= require('../../config/config');
+
+exports.saveAttachment = function(req, res){
+	console.log('controller/attachment post: ' + req.files[0].path );
+
+	// var newLocation = config.root + '/public/files/' + req.files[0].name;
+	// console.log('controller/attachment newLocation: ' + newLocation);
+
+	fs.readFile(req.files[0].path, function (err, data) {
+
+ 		var bucket = 'wmga';
+
+		var s3 = new AWS.S3();
+		s3.putObject({
+			ACL: 'public-read', // by default private access
+			Bucket: bucket,
+			Key: req.files[0].name,
+			Body: data
+		}, function (err, data) {
+			if (err) {
+				console.log(err);
+				res.send(500, {msg: 'image upload failed', error: err})
+			} else {
+				console.log('S3 upload Successful');
+				res.send({
+					path: 'https://s3-us-west-2.amazonaws.com/wmga/' + req.files[0].name,
+					fileName: req.files[0].name
+				});			
+			}
+		});
+	});
+};
