e7747b10de5c9103590835fd36084750e81a2bb0
Nick Nance <nance.nick@gmail.com>
2015-02-01 07:36:47 -0600

    remove server port in production settings for heroku

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -9,10 +9,6 @@ var prodConfig = {
   database: {
     // URL to connect to database
     url: 'mongodb://nodejitsu_nancenick:9sbirh3oi9li8bhhavccb4ov3q@ds061558.mongolab.com:61558/nodejitsu_nancenick_nodejitsudb2721215477'
-  },
-  server: {
-    // Port to run server on
-    port: 80
   }
 };
 
