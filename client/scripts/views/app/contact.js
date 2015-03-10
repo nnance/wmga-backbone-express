f90fef00a45bf507df1060bd90c62475f191dbc2
Nick Nance <nance.nick@gmail.com>
2015-03-09 23:34:59 -0500

    remove unneeded code for side bar menu on contacts

--- client/scripts/views/app/contact.js
+++ client/scripts/views/app/contact.js
@@ -4,13 +4,5 @@ define(function(require) {
   var BaseView = require('client/scripts/views/viewbase');
   return BaseView.extend({
     template: JST['client/templates/app/contact.jst'],
-
-    // onRender: function() {
-    //   this.$('#sidebar').affix({
-    //     offset: {
-    //       top: this.$('#sidebar').offset().top - 50
-    //     }
-    //   });
-    // }
   });
 });
