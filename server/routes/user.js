8aa0391ee6311a03fa864805630bd5a3041397d7
Nick Nance <nick.nance@switch.co>
2016-01-24 06:48:21 -0800

    fix delete user

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,6 +10,7 @@ var routes = function(app) {
   app.get('/rest/users', User.getList);
   app.get('/rest/users/:id', User.getById);
   app.post('/rest/users', User.addUser);
+  app.delete('/rest/users/:id', User.deleteUser);
   app.post('/rest/users/email', User.emailAllUsers);
   app.post('/rest/users/notify', User.notifyAllUsers);
   app.put('/rest/users/:id', User.updateUser);
