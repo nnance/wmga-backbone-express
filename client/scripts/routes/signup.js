fbcf9542a9147b0b0f1dbb7c5ebcb8e3c7a88d44
Nick Nance <nance.nick@gmail.com>
2016-01-31 16:37:11 -0800

    fix lint errors

--- client/scripts/routes/signup.js
+++ client/scripts/routes/signup.js
@@ -1,57 +1,58 @@
 define(function(require) {
-    'use strict';
+  'use strict';
 
-    var UserModel = require('client/scripts/models/users-model');
-    var IndexView = require('client/scripts/views/signup/index');
-    var TypeStepView = require('client/scripts/views/signup/typestep');
-    var EmailStepView = require('client/scripts/views/signup/emailstep');
-    var PasswordStepView = require('client/scripts/views/signup/passwordstep');
-    var CreateStepView = require('client/scripts/views/signup/createstep');
-    var PayNowStepView = require('client/scripts/views/signup/paynowstep');
-    var PayNowCreatedView = require('client/scripts/views/signup/paynowcreatedstep');
+  var Backbone = require('backbone');
+  var UserModel = require('client/scripts/models/users-model');
+  var IndexView = require('client/scripts/views/signup/index');
+  var TypeStepView = require('client/scripts/views/signup/typestep');
+  var EmailStepView = require('client/scripts/views/signup/emailstep');
+  var PasswordStepView = require('client/scripts/views/signup/passwordstep');
+  var CreateStepView = require('client/scripts/views/signup/createstep');
+  var PayNowStepView = require('client/scripts/views/signup/paynowstep');
+  var PayNowCreatedView = require('client/scripts/views/signup/paynowcreatedstep');
 
-    return Backbone.Router.extend({
-      routes: {
-        'signup': 'showSignup',
-        'signup/:step': 'showSignup',
-      },
+  return Backbone.Router.extend({
+    routes: {
+      'signup': 'showSignup',
+      'signup/:step': 'showSignup'
+    },
 
-      initialize: function(options) {
-        this.container = options.container;
-        this.dataManager = options.dataManager;
-        this.session = options.dataManager.session;
-        this.collection = options.dataManager.userCollection;
-        this.model = new UserModel();
+    initialize: function(options) {
+      this.container = options.container;
+      this.dataManager = options.dataManager;
+      this.session = options.dataManager.session;
+      this.collection = options.dataManager.userCollection;
+      this.model = new UserModel();
 
-        this.indexView = new IndexView();
-        this.views = {
-          welcome: TypeStepView,
-          email: EmailStepView,
-          password: PasswordStepView,
-          create: CreateStepView,
-          paynow: PayNowStepView,
-          createpaynow: PayNowCreatedView
-        };
-      },
+      this.indexView = new IndexView();
+      this.views = {
+        welcome: TypeStepView,
+        email: EmailStepView,
+        password: PasswordStepView,
+        create: CreateStepView,
+        paynow: PayNowStepView,
+        createpaynow: PayNowCreatedView
+      };
+    },
 
-      showSignup: function(index) {
-        var step = !index ? 'welcome' : index;
-        var ViewType = this.views[step];
+    showSignup: function(index) {
+      var step = !index ? 'welcome' : index;
+      var ViewType = this.views[step];
 
-        if (step !== 'welcome' && !this.model.has('existingMember')) {
-          Backbone.history.navigate('#signup', true);
-        } else {
-          if (step === 'welcome') {
-            this.model = new UserModel();
-            this.container.setView(this.indexView);
-          }
-          var view = new ViewType({model: this.model, collection: this.collection, session: this.session, dataManager: this.dataManager});
-          this.indexView.removeSubViews();
-          this.indexView.addSubView({
-            view: view,
-            selector: '#stepContent'
-          });
+      if (step !== 'welcome' && !this.model.has('existingMember')) {
+        Backbone.history.navigate('#signup', true);
+      } else {
+        if (step === 'welcome') {
+          this.model = new UserModel();
+          this.container.setView(this.indexView);
         }
-      },
-    });
+        this.indexView.removeSubViews();
+        var view = new ViewType({model: this.model, collection: this.collection, session: this.session, dataManager: this.dataManager});
+        this.indexView.addSubView({
+          view: view,
+          selector: '#stepContent'
+        });
+      }
+    }
   });
+});
